c291ab476a36f5740230b2bf8b398bf62ba2d5a7
Charles-n1 <charles.nguyen@epitech.eu>
2025-12-15 16:33:01 +0100

    [STYLE + ADD] J'ai add mon ancien projet dans le fichier alpha_version.

--- Front_end.py
+++ Front_end.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import sqlite3
+
 
 app = Flask(__name__)
+db = sqlite3.connect("Base_de_donnée.db")
+cursor = db.cursor()
 
 @app.route("/")
-def hello_world():
-    return render_template("doc.html")
+def insert_test():
+    return 
