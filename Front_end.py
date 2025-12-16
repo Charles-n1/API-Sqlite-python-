f88e12ba8de911953e2fdd1b51d6f9ea0618a25b
Charles-n1 <charles.nguyen@epitech.eu>
2025-12-16 15:08:16 +0100

    [UPT] Mon read : J'arrive à récupérer les données de ma base HTML, et de comprende le concepte derrière les méthodes

--- Front_end.py
+++ Front_end.py
@@ -5,7 +5,7 @@
 ## Front_end
 ##
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -19,12 +19,21 @@ def get_db():
 def main():
     return "hello BIRTHDAY"
 
-@app.route("/read")
+@app.route("/show_bd")
 def Show_all(): #Littéralement la même, sauf..
     cursor = get_db() #Je dois juste accéder à la DB
     cursor.execute("SELECT * FROM pc")
     rows = cursor.fetchall()
     return render_template("doc.html", info_table=rows)
+
+@app.route("/read", methods=["GET", "POST"])
+def read_function():
+    if request.method == "POST":
+        name = request.form.get("name")
+        # le code sqlite3
+        return "hello dude"
+    return render_template("read.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
